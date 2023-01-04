362621916a88e9b9479fe32335f850a01a37dba3
Jesier <jesiersantiago@gmail.com>
2023-01-04 14:25:48 -0600

    Asp ch 2 starting

---
+++ Repositories/IOwnerRepository.cs
@@ -0,0 +1,16 @@
+﻿using DogGo.Models;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace DogGo.Repositories
+{
+    public interface IOwnerRepository
+    {
+        List<Owner> GetAllOwners();
+        public void AddOwner(Owner owner);
+        Owner GetOwnerById(int id);
+
+        public void UpdateOwner(Owner owner);
+        public void DeleteOwner(int ownerId);
+    }
+}
